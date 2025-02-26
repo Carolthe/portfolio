669c4285030bc418f60ae860db5b5bbeb0e8648b
caroline <carololiveiratenorio21@gmail.com>
2025-02-26 17:06:23 +0000

    portfolio

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { FaBars  } from "react-icons/fa6"
-import Blod from '../imgs/Blod.png'
+
 import Blod2 from '../imgs/Blod2.png'
 
 export default function Home (){
@@ -11,15 +11,15 @@ export default function Home (){
                 <button className="text-[#f5e213] text-[30px] sm:hidden"> <FaBars  /> </button>
                 </div>
             </div>
-            <div className="sm:flex sm:justify-center sm:gap-[100px] sm:pt-[110px] ">
+            <div className="sm:flex sm:justify-center sm:gap-[120px] sm:pt-[110px] ">
             <div className='grid justify-items-center items-center pt-[100px] sm:pt-0 sm:h-[50px] sm:mt-[90px]'>
                 <h1 className='font-bold text-[#2a2a2a] mt-[20%] flex text-center text-[40px] sm:ml-[33px] '>Caroline Tenorio</h1>
                 <p className='text-[#666666] text-[19px]'>Desenvolvedora de Software</p>
                 <button className='w-[270px] h-[60px] text-[19px] bg-[#73d6ef] text-white mt-[30px] mb-[20px] '>Contact-me</button>
             </div>
-            <div className="h-[50%] mt-[5%] grid justify-items-center">
-                <img className="w-[80%]" src={Blod} />
-                <img className="relative right-[60px] bottom-[300px]" src={Blod2} />
+            <div className=" mt-[7%] ">
+                <div id="img-home"> <img className="" src={Blod2} />  </div>
+                
             </div>
             </div>
         </div>
