0493eccef22427bd5a44c1ad36dfed48f9e0c903
caroline <carololiveiratenorio21@gmail.com>
2025-02-27 21:17:10 +0000

    portfolio

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,37 @@
-import { FaBars  } from "react-icons/fa6"
+import { FaBars, FaX  } from "react-icons/fa6"
 
 import Blod2 from '../imgs/Blod2.png'
+import { useState } from "react"
 
 export default function Home (){
+
+    const [menuAberto, setMenuAberto] = useState(false)    
+
+    function menuOpen (){
+        setMenuAberto(!menuAberto)
+    }
+
+
     return(
         <div className='h-[100vh]'>
             <div className='flex justify-between items-center w-[100vw] p-[30px] bg-[#fbfbfb] border-b fixed sm:py-[10px] sm:px-[150px] sm:w-[100%] '>
                 <p className="text-[#f5e213] text-[25px] sm:text-[30px] " > CT </p>
                 <div>
-                <button className="text-[#f5e213] text-[30px] sm:hidden"> <FaBars  /> </button>
+                    {!menuAberto ?
+                <button onClick={menuOpen} className="text-[#f5e213] text-[30px] sm:hidden  "> <FaBars  /> </button>
+                : <button onClick={menuOpen} className="text-[#f5e213] text-[30px] sm:hidden "  > <FaX /> </button>  }
+                <div className={`fixed top-0 left-0 h-[400px] w-[270px] rounded-e-[30px] bg-white text-[#8d8d8c] text-[20px] text-center pt-[35px] transition-transform duration-300 ${
+          menuAberto ? 'transform translate-x-0' : 'transform -translate-x-full  '
+        }`}>
+            
+            <p>Home</p>
+            <p>Ferramentas</p>
+            <p>Projetos</p>
+            <p>Contactos</p>
+            <p>Tradução Eng</p>
+            <p>Modo Noite</p>
+            
+                </div>
                 </div>
             </div>
             <div className="sm:flex sm:justify-center sm:gap-[120px] sm:pt-[110px] ">
